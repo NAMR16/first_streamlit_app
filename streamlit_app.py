a4de62273a8ed04f0f2f6e6eba22427521e1b82e
NAMR16 <136085349+NAMR16@users.noreply.github.com>
2023-06-09 17:53:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
 streamlit.title("My parents new healthy dinner")
+streamlit.header("Breakfast menu")
 streamlit.text("Omega 3 & blueberry oatmeal")
 streamlit.text("Kale,spinach and rocket smothie")
